6fcc6417fa361ac07f48e715567552a897ccb5f7
jiangqianghua <jiangqianghua@jiangqianghuadeMacBook-Pro.local>
2018-07-22 22:44:53 +0800

    添加网络分布式外排序

--- extrnalsort/sort.go
+++ extrnalsort/sort.go
@@ -4,13 +4,24 @@ import(
 	"os"
 	"github.com/myproject/bingfa/pipeline"
 	"bufio"
+	"strconv"
 	)
+/**
 func main(){
 	infile := "small.in" 
 	outfile := "small.out"
 	p := createPipeline(infile,512,4)
 	writeToFile(p,outfile)
 	//printFile(outfile)
+}
+**/
+
+func main(){
+	infile := "small.in" 
+	outfile := "small.out"
+	p := createNetworkPipeline(infile,512,4)
+	writeToFile(p,outfile)
+	printFile(outfile)
 }
 
 func printFile(filename string){
@@ -54,6 +65,33 @@ func createPipeline(filename string ,fileSize,chunkCount int) <-chan int{
 			bufio.NewReader(file),chunkSize)
 		sortResults = append(sortResults,
 			pipeline.InMemSort(source))
+	}
+	return pipeline.MergeN(sortResults...)
+}
+
+// 以下是网络版本
+func createNetworkPipeline(filename string ,fileSize,chunkCount int) <-chan int{
+	chunkSize := fileSize / chunkCount
+	pipeline.Init()
+	sortAddr := []string{}
+	for i := 0 ; i < chunkCount ; i++{
+		file ,err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+		file.Seek(int64(i * chunkSize),0)
+		source := pipeline.ReaderSource(
+			bufio.NewReader(file),chunkSize)
+		addr := ":"+strconv.Itoa(7000+i)
+		// 塞给网络服务器
+		pipeline.NetworkSink(addr,pipeline.InMemSort(source))
+		sortAddr = append(sortAddr,addr)
+	}	
+	// 从网络服务器取
+	sortResults :=[] <-chan int{}
+	for _,addr := range sortAddr{
+		sortResults = append(sortResults,
+			pipeline.NetworkSource(addr))
 	}
 	return pipeline.MergeN(sortResults...)
 }
